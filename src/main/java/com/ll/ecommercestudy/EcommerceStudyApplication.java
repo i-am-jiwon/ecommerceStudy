1513126961496ab53fa71de8c5b9a75e5c9759d7
i-am-jiwon <dlark1128@gmail.com>
2023-12-04 16:40:00 +0900

    setting

---
+++ src/main/java/com/ll/ecommercestudy/EcommerceStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.ecommercestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/ecommercestudy/EcommerceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ecommercestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
